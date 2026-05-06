8ff836f894b14c887fd445b6cab0ee09b9dc6a04
mamibuflo <alana.moreira.pinto@escola.pr.gov.br>
2026-05-06 09:24:45 -0300

    .

--- java.script.js
+++ java.script.js
@@ -1,21 +1,7 @@
-// Função para adicionar prática à lista
+// Adicionar prática sustentável
 document.getElementById('btnAdicionar').addEventListener('click', () => {
     const pratica = prompt("Digite uma nova prática sustentável:");
     if (pratica) {
         const li = document.createElement('li');
         li.textContent = pratica;
-        document.getElementById('lista-praticas').appendChild(li);
-    }
-});
-
-// Função para enviar formulário
-document.getElementById('formContato').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const nome = document.getElementById('nome').value;
-    const mensagem = document.getElementById('mensagem').value;
-
-    document.getElementById('respostaForm').textContent = `Obrigado, ${nome}! Sua mensagem foi recebida: "${mensagem}"`;
-
-    // Limpa o formulário
-    this.reset();
-});
+        document.getElementById('impacto
